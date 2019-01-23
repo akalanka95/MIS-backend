c6a584eb7bf978dadf85232f10f20b674eefb1cc
akalanka95 <akalankamaduka95@gmail.com>
2019-01-23 15:02:52 +0530

    Employee

--- src/main/java/com/epic/MIS/MisApplication.java
+++ src/main/java/com/epic/MIS/MisApplication.java
@@ -1,14 +1,53 @@
 package com.epic.MIS;
 
+import com.epic.MIS.model.Bank;
+import com.epic.MIS.model.Employee;
+import com.epic.MIS.repository.BankRepository;
+import com.epic.MIS.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MisApplication {
+public class MisApplication implements CommandLineRunner {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private BankRepository bankRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MisApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Employee e1 = new Employee();
+        e1.setId(1);
+        e1.setFirstName("Akalankaa");
+        e1.setLastName("Gamage");
+        e1.setEmail("akala@gmail.com");
+        e1.setPassword("admin");
+        e1.setRole("employee");
+        employeeRepository.save(e1);
+
+        Bank b1 = new Bank();
+        b1.setBankName("HNB");
+        b1.setAmount(100);
+        bankRepository.save(b1);
+
+        Bank b2 = new Bank();
+        b2.setBankName("BOC");
+        b2.setAmount(150);
+        bankRepository.save(b2);
+
+        Bank b3 = new Bank();
+        b3.setBankName("DFCC");
+        b3.setAmount(200);
+        bankRepository.save(b3);
+    }
 }
 
--- src/main/java/com/epic/MIS/controller/EmployeeController.java
+++ src/main/java/com/epic/MIS/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.epic.MIS.controller;
 import com.epic.MIS.model.Employee;
 import com.epic.MIS.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "employee")
+@RequestMapping(value = "/api/employee")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
